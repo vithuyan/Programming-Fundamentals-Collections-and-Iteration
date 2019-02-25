7d0e47765369c27f9f89f9470904e2be8188ce40
Vithuyan Sivananthan <vithuyan.s@outlook.com>
2019-02-25 01:11:42 -0500

    done

---
+++ exercise.py
@@ -0,0 +1,41 @@
+#Exercise 0
+fav_colours = ["green","red","blue"]
+age_friends = [22,23,25]
+coin_flips = ["heads","heads","heads","tails","heads"]
+fav_artists = ["Burno","Ed","Jason"]
+
+word_define = {'apple' : 'a fruit',
+'carrot' : 'a vegatable', 'Honda' : 'a car'}
+fav_movie : {'Batman' : '1997', 'Heat' : '1998', 'Turnpoint' : '2007'}
+city_pop = {'New-York' : '50000000', 'Boston' : '90000000', 'Dallas' : '70000000'}
+name_friends = {'Bill' : '25', 'Joe' : '21', 'Mark' : '23'}
+
+
+
+
+
+#Exercise 1
+print(coin_flips)
+print(fav_colours[0])
+print(age_friends)
+age_friends.append(0)
+print(age_friends)
+print(fav_movie)
+
+#Exercise 2
+print(fav_colours[2])
+city_pop ['Toronto'] = 10000000
+print(city_pop)
+coin_flips.reverse()
+print(coin_flips)
+print(city_pop['Boston'])
+
+
+
+
+#Exercise 3
+print(fav_artists[0])
+print(fav_artists[1])
+age_friends.sort(), age_friends.reverse()
+print(age_friends)
+fav_movie['Batman']
